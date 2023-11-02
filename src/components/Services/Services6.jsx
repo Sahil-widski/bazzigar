435cfd0a1e1ea6f1cb00ddfdc7d41939624e26a3
Sahil-widski <sahilshah@widski.com>
2023-11-02 14:26:18 +0530

    new commit

--- src/components/Services/Services6.jsx
+++ src/components/Services/Services6.jsx
@@ -13,9 +13,16 @@ function Services6() {
             <h6 className="numb">01</h6>
             <h5>Super Spade</h5>
             <p>
-            Super Spade is not just a card game; it's an exhilarating journey into the world of strategy, skill, and suspense. With its roots in the classic game of Spades, Super Spade takes the thrill to a whole new level, offering a unique and captivating experience for players of all levels.
+              ` Super Spade is not just a card game; it's an exhilarating
+              journey into the world of strategy, skill, and suspense. With its
+              roots in the classic game of Spades, Super Spade takes the thrill
+              to a whole new level, offering a unique and captivating experience
+              for players of all levels.`
             </p>
-            <Link className="custom-font more main-color" href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID">
+            <Link
+              className="custom-font more main-color"
+              href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID"
+            >
               Create ID
             </Link>
           </div>
@@ -27,10 +34,16 @@ function Services6() {
             <h6 className="numb">02</h6>
             <h5>Cock Fight</h5>
             <p>
-            Cockfighting is a blood sport involving roosters ("cocks"), held in a ring called a cockpit. The first documented use of the word gamecock, denoting use of the cock as to a "game", a sport, pastime or entertainment, was recorded in 1634
+              ` Cockfighting is a blood sport involving roosters ("cocks"), held
+              in a ring called a cockpit. The first documented use of the word
+              gamecock, denoting use of the cock as to a "game", a sport,
+              pastime or entertainment, was recorded in 1634`
             </p>
-            <Link className="custom-font more main-color" href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID">
-             Create ID
+            <Link
+              className="custom-font more main-color"
+              href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID"
+            >
+              Create ID
             </Link>
           </div>
 
@@ -41,9 +54,16 @@ function Services6() {
             <h6 className="numb">03</h6>
             <h5>Lucky Streak</h5>
             <p>
-            Welcome to Lucky Streak, your passport to a world of thrilling casino entertainment, luck, and excitement. At Lucky Streak, we're dedicated to providing you with an unforgettable gaming experience that combines innovation, security, and an array of top-quality games.
+              ` Welcome to Lucky Streak, your passport to a world of thrilling
+              casino entertainment, luck, and excitement. At Lucky Streak, we're
+              dedicated to providing you with an unforgettable gaming experience
+              that combines innovation, security, and an array of top-quality
+              games.`
             </p>
-            <Link className="custom-font more main-color" href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID">
+            <Link
+              className="custom-font more main-color"
+              href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID"
+            >
               Create ID
             </Link>
           </div>
@@ -55,9 +75,16 @@ function Services6() {
             <h6 className="numb">04</h6>
             <h5>Evolution</h5>
             <p>
-            Welcome to Evolution Casino, where the world of online gaming evolves into a realm of entertainment, excitement, and endless possibilities. At Evolution Casino, we pride ourselves on offering a dynamic gaming experience, backed by innovation, security, and a diverse array of top-quality games.
+              ` Welcome to Evolution Casino, where the world of online gaming
+              evolves into a realm of entertainment, excitement, and endless
+              possibilities. At Evolution Casino, we pride ourselves on offering
+              a dynamic gaming experience, backed by innovation, security, and a
+              diverse array of top-quality games.`
             </p>
-            <Link className="custom-font more main-color" href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID">
+            <Link
+              className="custom-font more main-color"
+              href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID"
+            >
               Create ID
             </Link>
           </div>
@@ -69,9 +96,16 @@ function Services6() {
             <h6 className="numb">05</h6>
             <h5>Vivo Gaming</h5>
             <p>
-            Welcome to Vivo Gaming, your gateway to a world of immersive online gaming experiences. At Vivo Gaming, we're dedicated to bringing the excitement of live casino games and cutting-edge technology together, ensuring you have a thrilling and interactive gaming adventure.
+              ` Welcome to Vivo Gaming, your gateway to a world of immersive
+              online gaming experiences. At Vivo Gaming, we're dedicated to
+              bringing the excitement of live casino games and cutting-edge
+              technology together, ensuring you have a thrilling and interactive
+              gaming adventure.`
             </p>
-            <Link className="custom-font more main-color" href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID">
+            <Link
+              className="custom-font more main-color"
+              href="https://api.whatsapp.com/send?phone=918983583948&text=I want to create ID"
+            >
               Create ID
             </Link>
           </div>
@@ -79,6 +113,6 @@ function Services6() {
       </div>
     </section>
   );
-};
+}
 
 export default Services6;
