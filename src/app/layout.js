20a24c7a90f560cb6f20310dedae92971b491e1f
Sahil-widski <sahilshah@widski.com>
2023-11-05 12:56:42 +0530

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,9 +7,9 @@ import "react-circular-progressbar/dist/styles.css";
 
 
 export const metadata = {
-  title: 'Vie',
-  description: 'Vie - Multi-Purpose React.js Next.js Template',
-  keywords: ['HTML5', 'Template', 'Vie', 'Multi-Purpose', 'themeforest'],
+  title: 'BazzigarExchange',
+  description: 'Win Big',
+  keywords: ['casino', 'betting', 'bazzigar', 'exchange', 'lottery', 'teen patti'],
   icons: {
     icon: "/assets/imgs/favicon.ico",
     shortcut: "/assets/imgs/favicon.ico"
